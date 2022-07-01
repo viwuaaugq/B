8f0d29358d5fe36c59caefeaf7a3b10bcc337abe
J5J5 <107280795+J5J5@users.noreply.github.com>
2022-07-02 01:31:08 +0300

    Update botinfo.py

--- botinfo.py
+++ botinfo.py
@@ -1,9 +1,17 @@
 import requests
 import telebot
 from telebot import types
+import flask
+import logging
+from flask import Flask,request
 
 tk = "5571315977:AAG7UZIUAAHqst4EkR-u7W3J4ApUn_d2hxE"
 bot = telebot.TeleBot(tk)
+
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
 @bot.message_handler(commands=['start'])
 def s(message):
     cc = types.InlineKeyboardMarkup()
@@ -39,4 +47,15 @@ def mm(message):
     """)
     except:
         bot.send_message(message.chat.id,"التوكن غلط")
-bot.polling()
+
+@server.route(f"/{tk}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+  
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://tokeninfobot2.herokuapp.com/"+str(tk))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

ee12baf928f0d1d705afd4c87b18ab5ad42eec9a
TXN <104720168+viwuaaugq@users.noreply.github.com>
2022-07-02 13:42:37 +0300

    Update botinfo.py

--- botinfo.py
+++ botinfo.py
@@ -57,5 +57,5 @@ def redirect_message():
   
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url="https://tokeninfobot2.herokuapp.com/"+str(tk))
+    bot.set_webhook(url="https://tokenbote.herokuapp.com/"+str(tk))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
